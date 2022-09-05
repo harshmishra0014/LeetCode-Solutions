102b1c1c3929ae2c2176575de3b3dda55ca6fb72
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2022-09-05 12:28:23 +0530

    Time: 293 ms (97.83%), Space: 18.4 MB (15.10%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -2,11 +2,16 @@ class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         f=[0]*60
         c=0
+       
+        #storing frequencies of remainder
         for i in time:
             f[i%60]+=1
         
+        #condition for pairin of numbers with 0 as remainder
         if f[0]>1:
             c=(f[0]*(f[0]-1))//2
+        
+        # condition for half of number
         if f[30]>1:
             c+=(f[30]*(f[30]-1))//2
         i=1
