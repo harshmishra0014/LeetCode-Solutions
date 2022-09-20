2f14b1f6b30bd4ee304c4405008fd942f6eebec3
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2022-09-20 12:14:36 +0530

    Time: 3062 ms (5.21%), Space: 29.3 MB (74.35%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -8,13 +8,13 @@ class Solution:
             prefix_sum.append(nums[i]+prefix_sum[i-1])
         
         for i in range(len(nums)):
-            if i==0:
+            if i==0:     # first element
                 left=0
                 right= (prefix_sum[len(nums)-1]-nums[i]) - (nums[i]*(n-1-i))
-            elif i==(n-1):
+            elif i==(n-1):  # last element 
                 right=0
                 left=(nums[i]*i) - (prefix_sum[i-1])
-            else:
+            else:  # remaining middle elements
                 left= (nums[i]*i) - (prefix_sum[i-1])
                 right= (prefix_sum[n-1]-prefix_sum[i]) - (nums[i]*(n-1-i))
                 
