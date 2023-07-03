430101271c88bfcdd2649d96cb0b364f9a05d6e6
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2023-07-03 15:24:29 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (58.03%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,14 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//TC: O(N)+O(N/2)
 class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp=new ListNode();
+        temp=head;
+        int count=0;
+        while(temp!=null){
+            temp=temp.next;
+            count++;
+        }
+        count=count/2+1;
+        temp=head;
+        for(int i=1;i<count;i++)
+            temp=temp.next;
+        return temp;
+    }
+}
+
+
+//TC: O(n/2)
+class Solution1 {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        
+        //when fast would be at end then slow would be at middle
         while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+            slow = slow.next;   //increasing slow b y one position
+            fast = fast.next.next;  //increasing fast by two positions
         }
         return slow;
     }
