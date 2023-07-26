2bd85c4043de026b933d1676d491fb2dce5b7178
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2023-07-26 23:08:41 +0530

    Time: 0 ms (100.00%), Space: 41 MB (68.91%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int low = 0, high = arr.length-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == target)
+                return mid;
+            //any one side would e sorted
+            //checking for left sorted
+            if(arr[low] <= arr[mid]){
+                //if target exists in left sorted
+                if(arr[low] <= target && target <= arr[mid]){
+                    high = mid-1;
+                }
+                else
+                    low = mid+1;
+            }
+            //checking for right sorted
+            else{
+                //if target exists in right side
+                if(arr[mid] <= target && target <= arr[high]){
+                    low = mid+1;
+                }
+                else
+                    high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
