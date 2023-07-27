d301cfe3e4d52dbf2c7ddfbb36bd1846ecb00489
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2023-07-27 22:55:41 +0530

    Time: 0 ms (100.00%), Space: 49.9 MB (81.59%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int singleNonDuplicate(int[] nums) {
         int low=0;
         int high=nums.length-2;
@@ -11,5 +11,38 @@ class Solution {
                 high=mid-1;
         }
         return nums[low];
+    }
+}
+
+class Solution {
+    /*
+    initially the numbers follow a (even, odd) trend that is first occurrence is at odd  index and second at even index
+    as soon as a single element is encountered this trend changes to (odd, even)
+     */
+    public int singleNonDuplicate(int[] nums) {
+        int l = nums.length;
+        if(l == 1)              // if only one element is present
+            return nums[0];
+        /*
+        checking for edge cases
+         */
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[l-1] != nums[l-2])
+            return nums[l-1];
+
+        int low = 1, high = l-2;
+        while(low <= high){
+            int mid = (low + high)/2;
+            //if middle element is not equal to either of its side it means that it is the single element
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+                return nums[mid];
+            //checking if element is on right side i.e. we are on left side
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 ==1 && nums[mid] == nums[mid - 1]))
+                low = mid + 1;
+            else
+                high = mid -1;      //element on left side
+        }
+        return -1;
     }
 }
