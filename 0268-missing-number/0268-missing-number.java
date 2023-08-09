e67ce37f607c07e6737d47267e0bf13d07d4a462
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2023-08-09 22:18:03 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (97.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution4 {
     public int missingNumber(int[] nums) 
     {
         int i=0;
@@ -22,5 +22,63 @@ class Solution {
         int temp=nums[a];
         nums[a]=nums[b];
         nums[b]=temp;
+    }
+}
+//brute
+class Solution1 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i <= n; i++){
+            int flag = 0;
+            for(int j = 0; j < n; j++){
+                if(nums[j] == i){
+                    flag = 1;
+                }
+            }
+            if(flag == 0)
+                return i;
+        }
+        return -1;
+    }
+}
+//better
+class Solution2 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int[] hash = new  int[n + 1];
+        Arrays.fill(hash, 0);
+        for(int i : nums)
+            hash[i]++;
+        for(int i = 0; i < hash.length; i++){
+            if (hash[i] == 0)
+                return i;
+        }
+        return -1;
+    }
+}
+
+//optimal1 using logic of sum of first n natural number
+class Solution3 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1)/2;            //sum of first n natural numbers
+
+        int sum2 = 0;
+        for(int i : nums)
+            sum2 += i;
+        return sum-sum2;
+    }
+}
+
+//optimal 2 using xor
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xor1 = 0, xor2 = 0;
+        for(int i = 0; i < n; i++){
+            xor1 = xor1 ^ nums[i];
+            xor2 = xor2 ^ (i+1);
+        }
+        return xor1 ^ xor2;
     }
 }
