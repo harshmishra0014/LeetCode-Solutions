a50dc4e6452e492392d2f5fb1f934b653910d772
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2024-02-06 12:56:27 +0530

    Time: 28 ms (35.75%), Space: 44.9 MB (49.17%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -4,15 +4,8 @@ class Solution {
         Map<Character,Integer> d=new HashMap<Character,Integer>();
         for(int i=0;i<s.length();i++)
         {   
-            if(!d.containsKey(s.charAt(i)))
-                d.put(s.charAt(i),1);
-            else
-                d.put(s.charAt(i),d.get(s.charAt(i))+1);
-                
+            d.put(s.charAt(i), d.getOrDefault(s.charAt(i), 0) + 1);
         }
-         // Set<Character> ss=d.keySet();
-         //    for(char c:ss)
-         //        System.out.println(c+":"+d.get(c));
         for(int i=0;i<s.length();i++)
         {
             if (d.get(s.charAt(i))==1)
