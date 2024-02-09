2038cc3f0d816450f0933e53a3034aec6fd7d8a5
Harsh Mishra <72810244+harshmishra0014@users.noreply.github.com>
2024-02-09 23:01:18 +0530

    Time: 13 ms (99.83%), Space: 43.3 MB (28.83%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -47,8 +47,9 @@ class Solution1 {
 
 /*
 iterative dp
+O(n^2)
 */
-class Solution {
+class Solution2 {
     Map<Integer, List<Integer>> endingSubset;       //to store largest subset at respective indices
     
     public List<Integer> largestDivisibleSubset(int[] nums) {
@@ -84,4 +85,43 @@ class Solution {
         return maxSubset;
     }
     
+}
+
+
+/*
+space optimized iterative dp
+*/
+class Solution {
+    
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        if(n == 0)  return new ArrayList<>();
+                
+        Arrays.sort(nums);      //to follow only one order
+        int[] parent = new int[n];
+        int[] endingSubsetSize = new int[n];
+        int maxLength = 0, maxIndex = -1;
+        
+        Arrays.fill(parent, -1);
+        Arrays.fill(endingSubsetSize, 1);
+        
+        List<Integer> maxSubset = new ArrayList<>();
+        for(int i = 0; i < n; i++){   
+            for(int k = 0; k < i; k++){
+                if(nums[i] % nums[k] == 0 && endingSubsetSize[i] < endingSubsetSize[k] + 1){
+                    endingSubsetSize[i] = endingSubsetSize[k] + 1;
+                    parent[i] = k;
+                }
+            }
+            if(endingSubsetSize[i] > maxLength){
+                maxLength = endingSubsetSize[i];
+                maxIndex = i;
+            }
+        }
+        while(maxIndex != -1){
+            maxSubset.add(nums[maxIndex]);
+            maxIndex = parent[maxIndex];
+        }
+        return maxSubset;
+    }
 }
